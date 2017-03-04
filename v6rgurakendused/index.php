6db8e52ae94f537376815394746d01e0064e4bf0
Marilyn Võsu <mvosu@itcollege.ee>
2017-03-04 14:31:45 +0200

    final

--- v6rgurakendused/index.php
+++ v6rgurakendused/index.php
@@ -30,6 +30,11 @@ Kliki mind!!!</button>
 <?php 
    include "php/tabel.php";
 ?>
+<p>
+ <a href="http://validator.w3.org/check?uri=referer">
+  <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+ </a>
+</p>
 </body>
 	
 </html>
--- v6rgurakendused/php/tabel.php
+++ v6rgurakendused/php/tabel.php
@@ -1,12 +1,25 @@
-<?php 
-    $host = "localhost";
-    $user = "test";
-    $pass = "t3st3r123";
-    $db = "test";
-
-    $l = mysqli_connect($host, $user, $pass, $db);
-    mysqli_query($l, "SET CHARACTER SET UTF8") or
-            die("Error, ei saa andmebaasi charsetti seatud");
-    ...
-    mysqli_close($l);
-?>
+<?php
+		// Prints the current PHP verison
+		echo "Current PHP version: " . phpversion() . "<br>";
+		$host = "localhost";
+    	$user = "test";
+    	$pass = "t3st3r123";
+   		$db = "test";
+    	// Create connection
+		$conn = new mysqli($host, $user, $pass, $db);
+		// Check connection
+		if ($conn->connect_error) {
+    		die("Connection failed: " . $conn->connect_error);
+		}
+		$sql = "SELECT ID, Eesnimi, Perekonnanimi FROM Andmed";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+    	// output data of each row
+    		while($row = $result->fetch_assoc()) {
+        		echo "ID: " . $row["ID"]. " - Eesnimi: " . $row["Eesnimi"]. " - Perekonnanimi: " . $row["Perekonnanimi"]."<br>";
+    		}
+    	} else {
+    		echo "No data to be found";
+		}
+		$conn->close();
+	?>
